df13a357bcd4b277ce9db2c3861e00dffd6d3ce5
hifdzimaulana34 <hifdzimaulana.19934@gmail.com>
2021-09-19 16:27:15 +0700

    Minggu3 Prak 2

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homechild');
+});
+Route::get('about', function () {
+    return view('about');
 });
